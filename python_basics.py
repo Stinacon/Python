6a27124ae6f47ee4081b8fa46b7fbd0889b34870
Stinacon <122473348+Stinacon@users.noreply.github.com>
2023-07-24 11:15:09 -0400

    math functions

--- python_basics.py
+++ python_basics.py
@@ -1,10 +1,28 @@
-# Data Types
-int
-float
-bool
-str
-list
-tuple
-set
-dict
-`
+# # Fundamental Data Types
+# int
+# float
+# bool
+# str
+# list
+# tuple
+# set
+# dict
+
+# # Classes --> custom types
+
+# # Specialized Data Types
+# # ^^^ for whenever we don't have the data type we need and we don't want to create our own, we can use these from Modules
+
+# None  # absence of value
+# print(type(2+4))
+# print(type(2/4))
+# print(5//4)
+# print(5 % 4)  # modulo - returns the remainder of the division
+
+
+# math functions - just google them :)
+# print(round(3.1))
+# print(abs(-20))  # absolute value
+
+
+# Developer Fundamentals
