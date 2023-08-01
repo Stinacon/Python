e249cd7ac53e3c1271fead8516334973917a7301
Stinacon <122473348+Stinacon@users.noreply.github.com>
2023-08-01 15:44:35 -0400

    string slicing

--- sect_3_code.py
+++ sect_3_code.py
@@ -1,10 +1,36 @@
-iq = 190
-user_age = iq/4
-a = user_age
+# iq = 190
+# user_age = iq/4
+# a = user_age
 
-print(a)
+# print(a)
 
-# Escape Sequence
-weather = "\t It\'s \"kind of\" sunny \n hope you have a good day!"
+# # Escape Sequence
+# weather = "\t It\'s \"kind of\" sunny \n hope you have a good day!"
 
-print(weather)
+# print(weather)
+
+
+# F strings
+# name = 'Johnny'
+# age = 55
+
+# print(f'hi {name}. You are {age} years old')
+
+# print('hi {1}. You are {0} years old'.format(name, age))
+
+# selfish = '01234567'
+# String Slicing
+
+# # [start:stop]
+# print(selfish[0:2])
+# print(selfish[0:7])
+# print(selfish[0:8])
+
+# # [start:stop:stepover]
+# print(selfish[0:8:2])
+# print(selfish[1:])
+# print(selfish[:5])
+# print(selfish[-1])
+# print(selfish[::-1])
+
+# Immutability
